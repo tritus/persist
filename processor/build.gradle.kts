5dcba5a6af16085f6345962311316a2ae83f4313
Tristan Ferré <tristan@go-electra.com>
2022-11-17 17:56:12 +0100

    add publication

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -16,3 +16,15 @@ dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.5.31-1.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.tritus.persist"
+            artifactId = "processor"
+            version = "0.1"
+
+            from(components["kotlin"])
+        }
+    }
+}
