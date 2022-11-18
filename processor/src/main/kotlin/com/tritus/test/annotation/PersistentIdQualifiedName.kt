c1ffa269280df02f4c64f02602d0e49a00eb2f5b
Tristan Ferré <tristan@go-electra.com>
2022-11-18 09:22:31 +0100

    internal

--- processor/src/main/kotlin/com/tritus/test/annotation/PersistentIdQualifiedName.kt
+++ processor/src/main/kotlin/com/tritus/test/annotation/PersistentIdQualifiedName.kt
@@ -1,4 +1,4 @@
 package com.tritus.test.annotation
 
-val persistentIdQualifiedName = PersistentId::class.qualifiedName
+internal val persistentIdQualifiedName = PersistentId::class.qualifiedName
     ?: throw IllegalArgumentException("PersistentId annotation should have a qualified name")
